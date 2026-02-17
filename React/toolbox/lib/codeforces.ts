b1a9afdf81a15907af58f92aa29f66b4e65f7cd3
Monotheist <masudrahman998@gmail.com>
2026-02-17 18:33:28 +0600

    Added Zod

--- React/toolbox/lib/codeforces.ts
+++ React/toolbox/lib/codeforces.ts
@@ -1,7 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import { z } from "zod";
+
+export type ActionsState = {
+    errors?: { handle?: string[] };
+    message?: string | null;
+    data?: any[];
+};
+
+const HandleSchema = z.object({
+    handle: z.string().min(3, "Too short!").max(24, "Too long!"),
+});
+
 export async function fetchUserSolvedProblems(handle: string) {
     const url = `https://codeforces.com/api/user.status?handle=${handle}`;
     const response = await fetch(url);
     const data = await response.json();
+
     if (data.status !== "OK") {
         throw new Error("Target not found on Codeforces.");
     }
@@ -18,4 +34,26 @@ export async function fetchUserSolvedProblems(handle: string) {
     });
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return Array.from(solvedMap.values());
+}
+
+export async function analyzeHandle(
+    prevState: ActionsState,
+    formData: FormData,
+): Promise<ActionsState> {
+    const validatedFields = HandleSchema.safeParse({
+        handle: formData.get("handle"),
+    });
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Target Identification Falied. Correct the input.",
+        };
+    }
+    const { handle } = validatedFields.data;
+    try {
+        const data = await fetchUserSolvedProblems(handle);
+        return { data, message: "Identification Successful" };
+    } catch (error) {
+        return { message: "User not found on Codeforces.", data: [] };
+    }
 }
